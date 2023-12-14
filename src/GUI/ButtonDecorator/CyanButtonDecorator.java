ce8390d04c05a048e32305de5eb10cb576ac5c6c
SaniaRL <saniarunnfors@gmail.com>
2023-12-14 16:29:35 +0100

    Decorator updated. Three subclasses.

---
+++ src/GUI/ButtonDecorator/CyanButtonDecorator.java
@@ -0,0 +1,10 @@
+package GUI.ButtonDecorator;
+
+import GUI.DayButton;
+import java.awt.*;
+
+public class CyanButtonDecorator extends ButtonDecorator {
+    public CyanButtonDecorator(DayButton button){
+        super(button, Color.CYAN);
+    }
+}
